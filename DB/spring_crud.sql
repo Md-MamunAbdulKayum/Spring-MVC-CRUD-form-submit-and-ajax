4487671f79df1b7c042104824203b352744982de
Mamun Kayum <souravcuetcse2010@gmail.com>
2020-04-20 02:50:28 -0400

    updated spring and hibernate libraries

--- DB/spring_crud.sql
+++ DB/spring_crud.sql
@@ -1,23 +1,4 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50715
-Source Host           : localhost:3306
-Source Database       : spring_crud
-
-Target Server Type    : MYSQL
-Target Server Version : 50715
-File Encoding         : 65001
-
-Date: 2018-03-03 16:34:56
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -30,7 +11,3 @@ CREATE TABLE `user` (
   `Status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of user
--- ----------------------------
